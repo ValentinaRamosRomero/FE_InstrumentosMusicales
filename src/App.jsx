c85866f4a05ec12f24c82a9fb6dbeb2a96582804
Valentina Ramos Romero <vramosr@unal.edu.co>
2025-02-19 13:03:48 -0500

    Actualizacion Carpetas App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./Pages/Home/Home.jsx";
-import Landing from "./Pages/Landing/Landing.jsx";
+import Home from "./pages/home/Home";
+import Landing from "./pages/landing/Landing";
 
 const App = () => {
   return (
