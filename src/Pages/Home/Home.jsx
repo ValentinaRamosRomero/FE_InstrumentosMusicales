e1b8c5b6739acc42376f85e3dcf078dbbff196ff
ghuinchot <ghuinchot@uni.pe>
2025-03-02 19:57:36 -0500

    cambios home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,18 +2,32 @@ import React from "react";
 import "./Home.css";
 import Footer from "../../Componentes/Footer/Footer";
 import Categoria from "../../Componentes/Categories/Categoria";
-import Producto from "../../Componentes/products/Producto";
+import Producto from "../../Componentes/Products/Producto";
 import Header from "../../Componentes/Header/Header";
 import Banner from "../../Componentes/Banner/Banner";
 import SearchBar from "../../Componentes/SearchBar/SearchBar";
 
-const Home = () => {
+// Recibimos las props de autenticación
+const Home = ({ isAuthenticated, userData, onLogout }) => {
   return (
     <>
       <div className="container">
-        <Header />
+        {/* Pasamos las props al Header */}
+        <Header 
+          isAuthenticated={isAuthenticated} 
+          userData={userData} 
+          onLogout={onLogout} 
+        />
         <Banner />
         <SearchBar/>
+        
+        {/* Opcional: Mensaje de bienvenida personalizado */}
+        {isAuthenticated && userData && (
+          <div className="welcome-message">
+            <h2>Bienvenido, {userData.nombre} {userData.apellido}</h2>
+          </div>
+        )}
+        
         <div className="categorias-container">
           <h1>Categorías</h1>
           <Categoria />
@@ -26,7 +40,7 @@ const Home = () => {
         <Footer />
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
